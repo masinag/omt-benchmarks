Fix some wrong results for schittkowski collection

QF_NRAT/gen-data/schittkowski/extract_opt.py
@@ -17,7 +17,7 @@ def extract_opt(filename: str) -> dict[int, np.float64]:
 
     with open(filename, 'r') as file:
         for line in file:
-            match = re.match(r"^\s*(\d+) &\s*\d+ &\s*\d+ &\s*\d+ &\s*\d+ &\s*\d+&\s*\d+ &\s*\d+ &\s*(-?\d\.\d+E[+-]\d+)",
+            match = re.match(r"^\s*(\d+) &\s*\d+ &\s*\d+ &\s*\d+ &\s*\d+ &\s*\d+&\s*\d+ &\s*\d+ &\s*(-?\d\.\d+E[+-]\d+|[+-]Infinity)",
                              line)
             if match:
                 test_number = int(match.group(1))