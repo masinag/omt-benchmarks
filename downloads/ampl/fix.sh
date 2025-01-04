2931b467d6463b472de6317596ab20828c5f0c41
Gabriele Masina <gabriele.masina@virgilio.it>
2025-01-04 13:43:06 +0100

    Fix ampl benchmark fix script when executed multiple times

--- downloads/ampl/fix.sh
+++ downloads/ampl/fix.sh
@@ -8,21 +8,21 @@ FILES_DIR=$DIR_BASE
 HS112_FILE="${FILES_DIR}/hs/hs112.mod"
 echo "Fixing ${HS112_FILE}..."
 if grep -q "x[4] + 2*x[5] + x[6] + x[7] = 1;" "$HS112_FILE"; then
-    sed -i 's/x[4] + 2*x[5] + x[6] + x[7] = 1;/x[4] + 2*x[5] + x[6] + x[7] = 0;/' "$HS112_FILE"
-    echo "Modification in hs112.mod completed."
+  sed -i 's/x[4] + 2*x[5] + x[6] + x[7] = 1;/x[4] + 2*x[5] + x[6] + x[7] = 0;/' "$HS112_FILE"
+  echo "Modification in hs112.mod completed."
 else
-    echo "Warning: Pattern not found in hs112.mod"
+  echo "Warning: Pattern not found in hs112.mod"
 fi
 
 # FIX s254.mod
 S254_FILE="${FILES_DIR}/s/s254.mod"
 echo "Fixing ${S254_FILE}..."
 
-if ! grep -q "x[3] >= 1" "$S254_FILE"; then
-    sed -i '/data;/i subject to cons3:\nx[3] >= 1;\n' "$S254_FILE"
-    echo "Added cons3 before data block in s254.mod."
+if ! grep -q "x\[3\] >= 1" "$S254_FILE"; then
+  sed -i '/data;/i subject to cons3:\nx[3] >= 1;\n' "$S254_FILE"
+  echo "Added cons3 before data block in s254.mod."
 else
-    echo "Warning: cons3 already exists in s254.mod"
+  echo "Warning: cons3 already exists in s254.mod"
 fi
 
 echo "fix.sh script executed successfully."
